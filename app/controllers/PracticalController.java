6054a8a20cb5cf08e19776223ced939dc060f8c7
Marijn Goedegebure <marijngoedegebure@gmail.com>
2014-05-16 11:00:06 +0200

    Fixed the viewing of a practical, secret doesn't work yet

--- app/controllers/PracticalController.java
+++ app/controllers/PracticalController.java
@@ -13,18 +13,26 @@ public class PracticalController extends SecuredController{
     @Security.Authenticated(Secured.class)
     public static Result register(long id, String secret) {
         Practical practicalToRender = Practical.findById(id);
-        if(practicalToRender.getSecret().equals(secret)) {
-            getUser().addPractical(practicalToRender);
-            return redirect(routes.PracticalController.viewPractical(practicalToRender.getId()));
+        if(practicalToRender != null) {
+            if (practicalToRender.getSecret().equals(secret)) {
+                getUser().addPractical(practicalToRender);
+                return redirect(routes.PracticalController.viewPractical(practicalToRender.getId()));
+            } else {
+                return redirect(routes.PracticalController.viewPractical(practicalToRender.getId()));
+            }
         }
+        // When practical does not exist, show correct error
         else {
-            return redirect(routes.PracticalController.viewPractical(practicalToRender.getId()));
+            return redirect(routes.Application.index());
         }
     }
 
     @Security.Authenticated(Secured.class)
     public static Result viewPractical(long id) {
         Practical practicalToRender = Practical.findById(id);
-        return ok(viewPractical.render(practicalToRender));
+        if(practicalToRender != null)
+            return ok(viewPractical.render(practicalToRender));
+        else
+            return redirect(routes.Application.index());
     }
 }
