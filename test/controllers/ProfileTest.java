6b16c6ed934ffba40dc4f8302d55bc37158c393c
Freek van Tienen <freek.v.tienen@gmail.com>
2014-05-20 15:06:05 +0200

    Added some tests

---
+++ test/controllers/ProfileTest.java
@@ -0,0 +1,235 @@
+package controllers;
+
+import com.google.common.collect.ImmutableMap;
+import models.*;
+import org.junit.Before;
+import org.junit.Test;
+import play.mvc.*;
+import play.test.WithApplication;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static play.test.Helpers.*;
+
+/**
+ * Created by Freek on 12/05/14.
+ */
+public class ProfileTest extends WithApplication {
+    @Before
+    public void setUp() {
+        start(fakeApplication(inMemoryDatabase(), fakeGlobal()));
+        TestHelper.loadYamlFile("test-data.yml");
+    }
+
+    @Test
+    public void viewProfileDenied() {
+        Result result = callAction(
+                routes.ref.Profile.view()
+        );
+
+        assertEquals(SEE_OTHER, status(result));
+        assertEquals("authentication.unauthorized", flash(result).get("error"));
+    }
+
+    @Test
+    public void viewProfileSuccess() {
+        User user = User.findByEmail("test@test.com");
+        Result result = callAction(
+                routes.ref.Profile.view(),
+                fakeRequest().withSession("user_id", user.getId().toString())
+        );
+
+        assertEquals(OK, status(result));
+        assertTrue(contentAsString(result).contains(user.getFullName()));
+        assertTrue(contentAsString(result).contains(user.getType().toString()));
+        assertTrue(contentAsString(result).contains(user.getEmail()));
+    }
+
+    @Test
+    public void editProfileDenied() {
+        Result result = callAction(
+                routes.ref.Profile.edit()
+        );
+
+        assertEquals(SEE_OTHER, status(result));
+        assertEquals("authentication.unauthorized", flash(result).get("error"));
+    }
+
+    @Test
+    public void editProfileSuccess() {
+        User user = User.findByEmail("test@test.com");
+        Result result = callAction(
+                routes.ref.Profile.edit(),
+                fakeRequest().withSession("user_id", user.getId().toString())
+        );
+
+        assertEquals(OK, status(result));
+        assertTrue(contentAsString(result).contains(user.getFirstName()));
+        assertTrue(contentAsString(result).contains(user.getLastName()));
+        assertTrue(contentAsString(result).contains(user.getEmail()));
+    }
+
+    @Test
+    public void saveProfileRequired() {
+        User user = User.findByEmail("test@test.com");
+        Result result = callAction(
+                routes.ref.Profile.save(),
+                fakeRequest()
+                        .withSession("user_id", user.getId().toString())
+                        .withFormUrlEncodedBody(ImmutableMap.of(
+                                "firstName", user.getFirstName(),
+                                "lastName", "",
+                                "email", "trolololo@trol.com"))
+        );
+
+        assertEquals(BAD_REQUEST, status(result));
+        assertTrue(contentAsString(result).contains(user.getFirstName()));
+        assertTrue(contentAsString(result).contains("trolololo@trol.com"));
+        assertTrue(contentAsString(result).contains("This field is required"));
+    }
+
+    @Test
+    public void saveProfileInvalidEmail() {
+        User user = User.findByEmail("test@test.com");
+        Result result = callAction(
+                routes.ref.Profile.save(),
+                fakeRequest()
+                        .withSession("user_id", user.getId().toString())
+                        .withFormUrlEncodedBody(ImmutableMap.of(
+                                "firstName", user.getFirstName(),
+                                "lastName", user.getLastName(),
+                                "email", "nonvalidemailaddress!@#"))
+        );
+
+        assertEquals(BAD_REQUEST, status(result));
+        assertTrue(contentAsString(result).contains(user.getFirstName()));
+        assertTrue(contentAsString(result).contains(user.getLastName()));
+        assertTrue(contentAsString(result).contains("nonvalidemailaddress!@#"));
+        assertTrue(contentAsString(result).contains("Valid email required"));
+    }
+
+    @Test
+    public void saveProfileEmailInUse() {
+        User user = User.findByEmail("test@test.com");
+        Result result = callAction(
+                routes.ref.Profile.save(),
+                fakeRequest()
+                        .withSession("user_id", user.getId().toString())
+                        .withFormUrlEncodedBody(ImmutableMap.of(
+                                "firstName", user.getFirstName(),
+                                "lastName", user.getLastName(),
+                                "email", "mistertest@test.com"))
+        );
+
+        assertEquals(BAD_REQUEST, status(result));
+        assertTrue(contentAsString(result).contains(user.getFirstName()));
+        assertTrue(contentAsString(result).contains(user.getLastName()));
+        assertTrue(contentAsString(result).contains("mistertest@test.com"));
+        assertTrue(contentAsString(result).contains("The email address is already registered"));
+    }
+
+    @Test
+    public void saveProfileSuccess() {
+        User user = User.findByEmail("test@test.com");
+        Result result = callAction(
+                routes.ref.Profile.save(),
+                fakeRequest()
+                        .withSession("user_id", user.getId().toString())
+                        .withFormUrlEncodedBody(ImmutableMap.of(
+                                "firstName", "NewFirst Name",
+                                "lastName", "New last Name",
+                                "email", "newemail@test.com"))
+        );
+        User newUser = User.findByEmail("newemail@test.com");
+
+        assertEquals(SEE_OTHER, status(result));
+        assertEquals("profile.saved", flash(result).get("success"));
+        assertEquals("NewFirst Name New last Name", newUser.getFullName());
+        assertEquals(user.getId(), newUser.getId());
+        assertEquals("newemail@test.com", newUser.getEmail());
+    }
+
+    @Test
+    public void saveProfileWrongPass() {
+        User user = User.findByEmail("test@test.com");
+        Result result = callAction(
+                routes.ref.Profile.save(),
+                fakeRequest()
+                        .withSession("user_id", user.getId().toString())
+                        .withFormUrlEncodedBody(ImmutableMap.of(
+                                "firstName", user.getFirstName(),
+                                "lastName", user.getLastName(),
+                                "email", user.getEmail(),
+                                "password", "veryGoodPassword",
+                                "passwordRepeat", "notTheSamePassword"))
+        );
+
+        assertEquals(BAD_REQUEST, status(result));
+        assertTrue(contentAsString(result).contains(user.getFirstName()));
+        assertTrue(contentAsString(result).contains(user.getLastName()));
+        assertTrue(contentAsString(result).contains(user.getEmail()));
+        assertTrue(contentAsString(result).contains("The passwords don&#x27;t match"));
+    }
+
+    @Test
+    public void saveProfileWrongPassShort() {
+        User user = User.findByEmail("test@test.com");
+        Result result = callAction(
+                routes.ref.Profile.save(),
+                fakeRequest()
+                        .withSession("user_id", user.getId().toString())
+                        .withFormUrlEncodedBody(ImmutableMap.of(
+                                "firstName", user.getFirstName(),
+                                "lastName", user.getLastName(),
+                                "email", user.getEmail(),
+                                "password", "short",
+                                "passwordRepeat", "short"))
+        );
+
+        assertEquals(BAD_REQUEST, status(result));
+        assertTrue(contentAsString(result).contains(user.getFirstName()));
+        assertTrue(contentAsString(result).contains(user.getLastName()));
+        assertTrue(contentAsString(result).contains(user.getEmail()));
+        assertTrue(contentAsString(result).contains("Minimum length is"));
+    }
+
+    @Test
+    public void saveProfileFailNone() {
+        User user = User.findByEmail("admin@test.com");
+        Result result = callAction(
+                routes.ref.Profile.save(),
+                fakeRequest()
+                        .withSession("user_id", user.getId().toString())
+                        .withFormUrlEncodedBody(ImmutableMap.of(
+                                "firstName", user.getFirstName(),
+                                "lastName", user.getLastName(),
+                                "email", user.getEmail(),
+                                "password", "myNewPassword",
+                                "passwordRepeat", "myNewPassword"))
+        );
+
+        assertEquals(SEE_OTHER, status(result));
+        assertEquals("profile.saved", flash(result).get("success"));
+        assertNull(User.authenticate(user.getEmail(), "myNewPassword"));
+    }
+
+    @Test
+    public void saveProfileSuccessPass() {
+        User user = User.findByEmail("test@test.com");
+        Result result = callAction(
+                routes.ref.Profile.save(),
+                fakeRequest()
+                        .withSession("user_id", user.getId().toString())
+                        .withFormUrlEncodedBody(ImmutableMap.of(
+                                "firstName", user.getFirstName(),
+                                "lastName", user.getLastName(),
+                                "email", user.getEmail(),
+                                "password", "myNewPassword",
+                                "passwordRepeat", "myNewPassword"))
+        );
+
+        assertEquals(SEE_OTHER, status(result));
+        assertEquals("profile.saved", flash(result).get("success"));
+        assertNotNull(User.authenticate(user.getEmail(), "myNewPassword"));
+    }
+}
