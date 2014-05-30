5b0ea98d25eb63477a350a98a109e84692f0b2fc
Marijn Goedegebure <marijngoedegebure@gmail.com>
2014-05-30 14:48:34 +0200

    mend

--- app/helpers/Recommendation.java
+++ app/helpers/Recommendation.java
@@ -1,7 +1,19 @@
 package helpers;
 
+import models.*;
+
 /**
  * Created by Marijn Goedegebure on 30-5-2014.
+ * Class that will be used for the generation of recommendations
  */
 public class Recommendation {
+    private static final int maxAmountOfRecommendations = 5;
+
+    public static void getRecommendations(Practical practical, PracticalGroup practicalGroup) {
+        
+    }
+
+    public static void getRecommendation() {
+
+    }
 }
