04e3d50094ab9d47b77dc22f4e28e2dda092d222
Marijn Goedegebure <marijngoedegebure@gmail.com>
2014-05-30 16:04:50 +0200

    Made a first random recommendation

--- app/helpers/Recommendation.java
+++ app/helpers/Recommendation.java
@@ -2,6 +2,10 @@ package helpers;
 
 import models.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by Marijn Goedegebure on 30-5-2014.
  * Class that will be used for the generation of recommendations
@@ -9,11 +13,17 @@ import models.*;
 public class Recommendation {
     private static final int maxAmountOfRecommendations = 5;
 
-    public static void getRecommendations(Practical practical, PracticalGroup practicalGroup) {
-        
+    public static List<PracticalGroup> getRandomRecommendations(Practical practical, User user) {
+        List<PracticalGroup> newRecommendations = new ArrayList<PracticalGroup>();
+        for (int i = 0; i < maxAmountOfRecommendations; i++) {
+            newRecommendations.add(getRandomRecommendation(practical.getPracticalGroups()));
+        }
+        return newRecommendations;
     }
 
-    public static void getRecommendation() {
-
+    public static PracticalGroup getRandomRecommendation(List<PracticalGroup> practicalGroups) {
+        Random rn = new Random();
+        int generatedNumber = rn.nextInt(practicalGroups.size());
+        return practicalGroups.get(generatedNumber);
     }
 }
