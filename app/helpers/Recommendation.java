3ea902618206037b5e92a977da57a0062a699149
Freek van Tienen <freek.v.tienen@gmail.com>
2014-06-11 15:50:19 +0200

    Recommendation based on distance

--- app/helpers/Recommendation.java
+++ app/helpers/Recommendation.java
@@ -2,9 +2,7 @@ package helpers;
 
 import models.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 import static com.avaje.ebean.Expr.eq;
 import static com.avaje.ebean.Expr.or;
@@ -16,9 +14,9 @@ import static com.avaje.ebean.Expr.or;
 public final class Recommendation {
 
     /**
-     * The maximum number of recommendations
+     * The amount of recommendations per page
      */
-    private static final int MAX_RECOMMENDATIONS = 5;
+    private static final int PAGE_SIZE = 5;
 
     /**
      * Method to prevent instantiation
@@ -30,178 +28,110 @@ public final class Recommendation {
     }
 
     /**
-     * Method to get random recommendations for the user
-     * @param practical to get recommendations for
-     * @param user to get recommendations for
-     * @return list of maxAmountOfRecommendations recommendations
+     * Calculates the new average in skills between 2 practical groups when they are combined
+     * @param practicalGroup1 the first practical group to combine
+     * @param practicalGroup2 the second practical group to combine
+     * @return the new calculated average in skills of the group
      */
-    public static List<PracticalGroup> getRandomRecommendations(Practical practical, User user) {
-        List<PracticalGroup> newRecommendations = new ArrayList<PracticalGroup>();
-        for (int i = 0; i < MAX_RECOMMENDATIONS; i++) {
-            newRecommendations.add(getRandomRecommendation(practical.getPracticalGroups()));
-        }
-        return newRecommendations;
-    }
+    public static HashMap<Skill, Double> average(PracticalGroup practicalGroup1, PracticalGroup practicalGroup2) {
+        HashMap<Skill, Double> result = new HashMap<Skill, Double>();
 
-    /**
-     * Method to return a single recommendation
-     * @param practicalGroups to select from
-     * @return random practical group
-     */
-    public static PracticalGroup getRandomRecommendation(List<PracticalGroup> practicalGroups) {
-        Random rn = new Random();
-        int generatedNumber = rn.nextInt(practicalGroups.size());
-        return practicalGroups.get(generatedNumber);
-    }
+        // Go trough all the skills
+        for(Skill skill : Skill.findAll()) {
+            List<SkillValue> uSkills = SkillValue.find.where().and(
+                    eq("skill.name", skill.getName()),
+                    or(
+                            eq("user.practicalGroups.id", practicalGroup1.getId()),
+                            eq("user.practicalGroups.id", practicalGroup2.getId())
+                    )
+            ).findList();
 
-    /**
-     * Method to get non random generated recommendations
-     * @param practical to find recommendations for
-     * @param user to find recommendations for
-     * @return list of recommendations
-     */
-    public static List<PracticalGroup> getRecommendations(Practical practical, User user) {
-        PracticalGroup practicalGroupOfUser = PracticalGroup.findWithPracticalAndUser(practical, user);
-        List<PracticalGroup> newRecommendations = comparePracticalGroupWithPractical(
-                practicalGroupOfUser, practical.getPracticalGroups());
-        return newRecommendations;
-    }
-
-    /**
-     * Method to compare the different practical groups of the practical with the practical group of the user
-     * @param practicalGroupOfUser to compare
-     * @param practicalGroups to compare to
-     * @return list of recommendations
-     */
-    public static List<PracticalGroup> comparePracticalGroupWithPractical(
-            PracticalGroup practicalGroupOfUser, List<PracticalGroup> practicalGroups) {
-        List<PracticalGroup> newRecommendations = new ArrayList<PracticalGroup>();
-        List<Integer> currentScores = new ArrayList<Integer>();
-        for(PracticalGroup practicalGroup : practicalGroups) {
-            if (!practicalGroupOfUser.equals(practicalGroup)) {
-                int difference = calculateDifferenceAverageAndGiven(practicalGroupOfUser, practicalGroup);
-                // get index where the number should be inserted into the list
-                int index = getIndexForAddition(currentScores, difference);
-                if (index != -1) {
-                    // Add the difference to the appropriate list
-                    currentScores = addIntegerToListGivenIndex(currentScores, index, difference);
-                    // Add the practical group to the appropriate list
-                    newRecommendations = addPracticalGroupToListGivenIndex(newRecommendations, index, practicalGroup);
-                }
+            // Calculate the average here through support of ebean is limited (maybe later fix RAW SQL)
+            double skillAverage = 0;
+            for(SkillValue skillValue: uSkills) {
+                skillAverage += skillValue.getValue();
             }
+
+            if(skillAverage > 0 && uSkills.size() > 0) {
+                skillAverage /= uSkills.size();
+            }
+
+            // Put the result in the HashMap
+            result.put(skill, skillAverage);
         }
-        return newRecommendations;
+
+        return result;
     }
 
     /**
-     * Method that compares the practical group of the user
-     * and another practical group and calculates a difference between these
-     * and a given list of values by the practical
-     * @param practicalGroupOfUser to compare
-     * @param practicalGroupToCompare to compare with
-     * @return difference between the combined practical groups and the given list
+     * Calculates the distance between 2 skill sets
+     * @param skills The practical groups' skills
+     * @param pSkills The practical required skills
+     * @return The distance between the practical groups' skills and the practical skills
      */
-    public static int calculateDifferenceAverageAndGiven(
-            PracticalGroup practicalGroupOfUser, PracticalGroup practicalGroupToCompare) {
-        List<Integer> listOfAverages = calculateAverageForGroupAndGroupUser(
-                practicalGroupOfUser, practicalGroupToCompare);
-        List<Integer> listOfPractical = new ArrayList<Integer>();
-        listOfPractical.add(1);
-        listOfPractical.add(1);
-        int difference = 0;
-        for (int i = 0; i < listOfAverages.size(); i++) {
-            difference += listOfPractical.get(i) - listOfAverages.get(i);
-        }
-        return difference;
-    }
+    public static double distance(HashMap<Skill, Double> skills, List<SkillValuePractical> pSkills) {
+        double average = 0;
 
-    /**
-     * Method to calculate the average of the user's practical group and another practical group
-     * @param practicalGroupOfUser to calculate the average of
-     * @param practicalGroupToCompare to calculate the average of
-     * @return list of integers with averages
-     */
-    public static List<Integer> calculateAverageForGroupAndGroupUser(
-            PracticalGroup practicalGroupOfUser, PracticalGroup practicalGroupToCompare) {
-        List<Skill> skills = Skill.findAll();
-        List<Integer> listOfAverages = new ArrayList<Integer>();
-        for(int i = 0; i < skills.size(); i++) {
-            listOfAverages.add(calculateAverageForSkill(skills.get(i), practicalGroupOfUser, practicalGroupToCompare));
+        // Go trough all the practicum skills
+        for(SkillValue pSkill : pSkills) {
+            average += Math.pow(pSkill.getValue() - skills.get(pSkill.getSkill()), 2);
         }
-        return listOfAverages;
-    }
 
-    /**
-     * Method to calculate the average of a practicalgroup given a skill
-     * @param skill that is given
-     * @param practicalGroupOfUser to calculate the average of
-     * @param practicalGroupToCompare to calculate the average of
-     * @return list of averages
-     */
-    public static int calculateAverageForSkill(
-            Skill skill, PracticalGroup practicalGroupOfUser, PracticalGroup practicalGroupToCompare) {
-        List<SkillValue> userSkillsPracticalGroup = SkillValue.find.where().and(
-                eq("skill.name", skill.getName()),
-                or(
-                        eq("user.practicalGroups.id", practicalGroupOfUser.getId()),
-                        eq("user.practicalGroups.id", practicalGroupToCompare.getId())
-                )
-        ).findList();
-        int average = 0;
-        for(SkillValue userSkill : userSkillsPracticalGroup) {
-            // calculate average
-            average += userSkill.getValue();
+        // Square the average over all the different skills
+        if(pSkills.size() > 0) {
+            average = Math.pow(average, 1 / (double) pSkills.size());
         }
-        average = average / userSkillsPracticalGroup.size();
+
         return average;
     }
 
     /**
-     * Method to add a difference on a specific index, the rest of the values are moved up one spot
-     * @param list to add to
-     * @param index to add on
-     * @param item to add
-     * @return list where an addition has been made
+     * Recommend practical groups to a certain user
+     * @param user The user that wants the recommendation
+     * @param practical The practical the user want the recommendation for
+     * @param page The page of the recommendation
+     * @return Recommendations based on the skills of the practical group
      */
-    public static List<Integer> addIntegerToListGivenIndex(List<Integer> list, int index, int item) {
-        for(int i = index; i < MAX_RECOMMENDATIONS -1 && i < list.size(); i++) {
-            list.set(i+1, list.get(i));
+    public static List<PracticalGroup> recommend(User user, Practical practical, int page) {
+        HashMap<PracticalGroup, Double> list = new HashMap<PracticalGroup, Double>();
+        PracticalGroup practicalGroup1 = PracticalGroup.findWithPracticalAndUser(practical, user);
+
+        // Go trough all the practical groups
+        for(PracticalGroup practicalGroup2 : practical.getPracticalGroups()) {
+            // Check if it is not our own group
+            if(practicalGroup1.equals(practicalGroup2)) {
+                continue;
+            }
+
+            // Calculate average and then the distance
+            HashMap<Skill, Double> average = average(practicalGroup1, practicalGroup2);
+            list.put(practicalGroup2, distance(average, practical.getSkills()));
         }
-        list.set(index, item);
-        return list;
+
+        // Sort the HashMap by values
+        LinkedList sortedList = sortByValues(list);
+
+
+        return sortedList.subList(
+                Math.min((page-1)*PAGE_SIZE, sortedList.size()),
+                Math.min(page*PAGE_SIZE, sortedList.size()));
     }
 
     /**
-     * Method to add a practical group on a specific index, the rest of the values are moved up one spot
-     * @param list to add to
-     * @param index to add on
-     * @param item to add
-     * @return list where an addition has been made
+     * Sort a HashMap by it's values
+     * @param map The HashMap to sort
+     * @return A sorted LinkedList containing the
      */
-    public static List<PracticalGroup> addPracticalGroupToListGivenIndex(
-            List<PracticalGroup> list, int index, PracticalGroup item) {
-        for(int i = index; i < MAX_RECOMMENDATIONS -1 && i < list.size(); i++) {
-            list.set(i+1, list.get(i));
-        }
-        list.set(index, item);
-        return list;
-    }
+    private static LinkedList<PracticalGroup> sortByValues(final HashMap<PracticalGroup, Double> map) {
+        LinkedList<PracticalGroup> list = new LinkedList<PracticalGroup>(map.keySet());
+        // Defined Custom Comparator here
+        Collections.sort(list, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) map.get(o1))
+                        .compareTo(map.get(o2));
+            }
+        });
 
-    /**
-     * Method to determine on which index the new practical group and difference should be added
-     * @param list to add to
-     * @param number to add
-     * @return index where to add
-     */
-    public static int getIndexForAddition(List<Integer> list, int number) {
-        for(int i = 0; i < list.size(); i++) {
-            if(list.get(i) == null) {
-                return i;
-            }
-            if(list.get(i) < number) {
-                return i;
-            }
-        }
-        return -1;
+        return list;
     }
 }
