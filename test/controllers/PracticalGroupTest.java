df310ebdbc22a3a31770dbf5eb6f56016206a82a
Freek van Tienen <freek.v.tienen@gmail.com>
2014-06-19 16:26:54 +0200

    Practical group tests

--- test/controllers/PracticalGroupTest.java
+++ test/controllers/PracticalGroupTest.java
@@ -36,14 +36,32 @@ public class PracticalGroupTest extends WithApplication {
 
     @Test
     public void viewNotEnrolled() {
-        List<PracticalGroup> groups = PracticalGroup.findByPractical(Practical.findByName("PracticalControllerTest"));
+        Practical practical = Practical.findByName("PracticalControllerTest");
+        PracticalGroup group = new PracticalGroup(practical, user);
+        group.save();
 
-        Result result = callAction(routes.ref.PracticalGroupController.view(groups.get(0).getId()),
+        Result result = callAction(routes.ref.PracticalGroupController.view(group.getId()),
                 fakeRequest().withSession("user_id",
                         user.getId().toString()));
 
         assertEquals(SEE_OTHER, status(result));
         assertEquals("practical.userIsNotEnrolled", flash(result).get("error"));
+    }
+
+    @Test
+    public void view() {
+        Practical practical = Practical.findByName("PracticalControllerTest");
+        PracticalGroup group = new PracticalGroup(practical, user);
+        group.save();
+        practical.addUser(user);
+        practical.save();
+
+        Result result = callAction(routes.ref.PracticalGroupController.view(group.getId()),
+                fakeRequest().withSession("user_id",
+                        user.getId().toString()));
+
+        assertEquals(OK, status(result));
+        assertTrue(contentAsString(result).contains(group.getOwner().getFirstName()));
     }
 
 }
